51143170251e76f560eca4341221d551e27f393c
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2023-03-14 18:41:47 +0200

    https

---
+++ src/api/getNews.js
@@ -0,0 +1,4 @@
+
+const APIkey = '6orwVgKbvG4w4tc3rUtwLDnGBQVGIJGuynBGLyq1lyw';
+const baseULR = '';
+
--- src/api/getWeather.jsx
+++ src/api/getWeather.jsx
@@ -2,7 +2,7 @@
 
 const APIkey = 'dada1f37ac1948d0aa8144651231403';
 
-const baseURL = 'http://api.weatherapi.com/v1';
+const baseURL = 'https://api.weatherapi.com/v1';
 
 export const getWeather = async (cityName) => {
   return await fetch(`${baseURL}/current.json?key=${APIkey}&q=${cityName}`).then(response => response.json());
