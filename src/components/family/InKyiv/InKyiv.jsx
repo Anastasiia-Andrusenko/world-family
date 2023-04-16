d21c2224f3b37bbbb432cc4fd037d5920c1ae41c
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2023-04-16 22:15:30 +0300

    fix style Kyiv card

--- src/components/family/InKyiv/InKyiv.jsx
+++ src/components/family/InKyiv/InKyiv.jsx
@@ -1,12 +1,13 @@
 import css from "../InKyiv/InKyiv.module.css";
 
 import nastia from "../../../img/nastia.png";
+import classNames from "classnames";
 
 export const InKyiv = ({onMembersClick}) => {
   return <div className={css.in__kyiv} id='InKyiv' onClick={onMembersClick}>
     <div className={css.link}>
       <ul className={css.list}>
-        <li className={css.item}>
+        <li className={classNames(css.item, css.alone)}>
           <img alt="nastia_photo" src={nastia} className={css.avatar}></img>
           <span className={css.name}>Nastia</span>  
         </li>
--- src/components/family/InPoland/InPoland.jsx
+++ src/components/family/InPoland/InPoland.jsx
@@ -1,13 +1,14 @@
 import css from "../InPoland/InPoland.module.css";
 
 import andrew from "../../../img/andrew.png";
+import classNames from "classnames";
 // import nastia from "../../../img/nastia.png";
 
 export const InPoland = ({onMembersClick}) => {
   return <div className={css.in__pl} id='InPoland' onClick={onMembersClick}>
     <div className={css.link}>
       <ul className={css.list}>
-        <li className={css.item}>
+        <li className={classNames(css.item, css.alone)}>
           <img alt="andrew_photo" src={andrew} className={css.avatar}></img>
           <span className={css.name}>Andrew</span>
         </li>
